1ad9d3883eb28b649671b1faab7f59d76ec15a43
Grekoff18 <kolagrekalo@gmail.com>
2021-04-18 22:35:10 +0300

    feat: add some logic

--- lib/main.dart
+++ lib/main.dart
@@ -15,9 +15,33 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () => setState(() => leftDiceNumber = 3),
+              child: Image.asset("images/dice$leftDiceNumber.png"),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () => print("Hello world!!!"),
+              child: Image.asset("images/dice1.png"),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
